89ea19f5e0ada5661a482896123a3eba4fae7512
unknown <muskyrahman@gmail.com>
2025-09-10 17:25:15 +0700

    first commit

--- mine.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const mine = () => {
-  return <div>mine</div>;
-};
-
-export default mine;
---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [new URL("http://localhost:3000/**")],
+  },
+};
+export default nextConfig;
